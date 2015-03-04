b6734c2213a9790db9e51c66625262b91304adfe
Chris Sweigard <sweigardc@gmail.com>
2015-03-04 17:09:31 -0500

    Revert to hard code

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -8,7 +8,7 @@ module.exports = {
     uri: 'mongodb://localhost/inscoop-dev'
   },
   sequelize: {
-    uri: 'DATABASE_URL',
+    uri: 'postgres://ojfmghuyqfnvfe:d10vHLtgdJAE4tIDTZVTN-uqr5@ec2-23-23-80-55.compute-1.amazonaws.com:5432/d32megn1lmgjvr',
     options: {
       logging: false,
       storage: 'dev.sqlite',
