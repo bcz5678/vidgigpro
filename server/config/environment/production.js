9457ed096fcbf2dd77bcbb0fd8657f9c8cb25c11
Chris Sweigard <sweigardc@gmail.com>
2015-03-04 17:37:52 -0500

    Remove Mongo from Prod config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -13,14 +13,7 @@ module.exports = {
             process.env.PORT ||
             8080,
 
-  // MongoDB connection options
-  mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL +
-            process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/inscoop'
-  },
+  // Sql 
   sequelize: {
     uri: 'postgres://ojfmghuyqfnvfe:d10vHLtgdJAE4tIDTZVTN-uqr5@ec2-23-23-80-55.compute-1.amazonaws.com:5432/d32megn1lmgjvr',
     options: {
